c309a37cba2f1245db13f38b74c4886e497b746b
marko1684 <marko@math.com>
2025-03-31 01:33:55 +0200

    Initialized server
    Created initial folder structure for the server and initialized db
    schema

---
+++ server/src/config/db.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const gameSchema = new mongoose.Schema({
+   game_id : {type : String, required: true, unique: true},
+   players : [{player_id: String, points : Number}],
+   board_state : {
+      player_positions: {type: Map, of: String},
+      diamond_position: {type: String},
+   },
+   last_move: {
+      player_id: String,
+      move_piece: String,
+      from: String,
+      to: String,
+      lied: Boolean,
+   },
+   status: {
+      type: String,
+      enum: ["waiting", "in_progress", "finished"], default: "waiting"
+   },
+});
+
+module.exports = mongoose.model("Game", gameSchema);
---
+++ server/src/routes/game_routes.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const router = express.Router();
+const game_controller = require("../controllers/game_controller");
+
+// routes 
+
+router.post("/create", game_controller.create_game);
+router.post("/join", game_controller.join_game);
+router.post("/make_move", game_controller.make_move);
+router.post("/challenge_move", game_controller.challenge_move);
+
+// I guess we will need it
+router.get("state/:gameId", game_controller.get_game_state);
+
+module.exports = router;

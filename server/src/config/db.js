7c540cdf968d28a3434947d3845503a9a3a7938e
marko1684 <marko@math.com>
2025-04-04 01:47:03 +0200

    format

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -4,14 +4,20 @@ const gameSchema = new mongoose.Schema({
     game_id: { type: String, required: true, unique: true },
     players: [{ player_id: String }],
     board_state: {
-        player_positions: { type: Map, of: { position: String, points: Number } },
+        player_positions: {
+            type: Map,
+            of: { position: String, points: Number },
+        },
         diamond_position: { type: String },
     },
     last_move: {
         player_id: String,
         lied: Boolean,
         board_state: {
-            player_positions: { type: Map, of: { position: String, points: Number } },
+            player_positions: {
+                type: Map,
+                of: { position: String, points: Number },
+            },
             diamond_position: { type: String },
         },
     },
@@ -21,6 +27,5 @@ const gameSchema = new mongoose.Schema({
         default: 'waiting',
     },
 });
-
 
 module.exports = mongoose.model('Game', gameSchema);
--- server/src/models/game_model.js
+++ server/src/models/game_model.js
@@ -15,6 +15,7 @@ const game_model = {
             throw new Error('Game not found');
         }
         current_game.last_move.board_state = current_game.board_state;
+        current_game.board_state = new_board_state;
         current_game.last_move.player_id = move.player_id;
         current_game.last_move.lied = move.lied;
         await current_game.save();
@@ -22,19 +23,19 @@ const game_model = {
     },
 
     async get_current_state(game_id) {
-        let current_game = await Game.findById(game_id); 
+        let current_game = await Game.findById(game_id);
         if (!current_game) {
-            throw new Error("Game not found");
+            throw new Error('Game not found');
         }
         return current_game;
     },
 
     async finish_game(game_id) {
-        let current_game = await Game.findById(game_id); 
+        let current_game = await Game.findById(game_id);
         if (!current_game) {
-            throw new Error("Game not found");
+            throw new Error('Game not found');
         }
-        current_game.status = "finished";
+        current_game.status = 'finished';
         await current_game.save();
         return current_game;
     },
@@ -42,22 +43,24 @@ const game_model = {
     async challenge_move(game_id, player1_id, player2_id) {
         let current_game = await Game.findById(game_id);
         if (!current_game) {
-            throw new Error("Game not found");
+            throw new Error('Game not found');
         }
-    
+
         if (current_game.last_move.lied === true) {
-            current_game.status = "challangedblabla"; 
-    
+            current_game.status = 'challangedblabla';
+
             current_game.last_move.lied = false;
             current_game.board_state = current_game.last_move.board_state;
-    
-            current_game.board_state.player_positions.get(player1_id).points += 1;
-            current_game.board_state.player_positions.get(player2_id).points -= 1;
+
+            current_game.board_state.player_positions.get(player1_id).points +=
+                1;
+            current_game.board_state.player_positions.get(player2_id).points -=
+                1;
         } else {
-            current_game.board_state.player_positions.get(player1_id).points -= 1;
+            current_game.board_state.player_positions.get(player1_id).points -=
+                1;
         }
-    
+
         await current_game.save();
-    }
-    
+    },
 };

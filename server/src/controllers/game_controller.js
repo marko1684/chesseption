cd5a8df9691092e713b428b75846fd94f1778c8c
marko1684 <mpetrovic.1684@gmail.com>
2025-04-30 16:04:10 +0200

    fixed merge error

--- server/src/controllers/game_controller.js
+++ server/src/controllers/game_controller.js
@@ -14,14 +14,8 @@ const gameController = {
 
     async make_move(req, res) {
         try {
-<<<<<<< HEAD
             const { game_id, player_id, lied, box, new_board_state } = req.body;
             const updated_game = await game_model.make_move(game_id, player_id, lied, box, new_board_state);
-=======
-            const { game_id, player_id, lied, new_board_state } = req.body;
-            const updated_game = await game_model.make_move(game_id, player_id, lied, new_board_state);
-<<<<<<< Updated upstream
->>>>>>> 3149376f764d6eefaa4518b61ca17f4a8b24aa90
 
             console.log('Request body for making a move:');
             console.log(JSON.stringify(req.body, null, 2));
@@ -29,11 +23,6 @@ const gameController = {
             console.log('Updated game after making a move:');
             console.log(JSON.stringify(updated_game, null, 2));
 
-=======
-            console.log(req.body);
-            console.log('qurac');
-            console.log(updated_game);
->>>>>>> Stashed changes
             res.json(updated_game);
         } catch (error) {
             res.status(500).json({ error: error.message });
